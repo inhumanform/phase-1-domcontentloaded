e1c0a1b15d02c4d8ca0788fe83644087d9c5b34e
Drew <dch528@gmail.com>
2024-02-02 10:33:52 -0500

    Drew Hairston DOM Content Loaded Lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Your code goes here
+
+// document.addEventListener('DOMContentLoaded', (event) => {
+//     const p = document.querySelector('p');
+//     if (p) {
+//         p.textContent = "This is really cool!";
+//     }
+// });
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelector('p').textContent = "This is really cool!";
+});
